e61295749e9065459565b7798a5037bda25758a2
irvinalfaro11 <irvinalfaro11@gmail.com>
2018-03-03 13:24:12 +0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './container/Main';
+import './style/_base-style.scss';
+
+ReactDOM.render(<Main />, document.getElementById('root'));
